afb2e8a669e046a67ca96e7cba63465ef2ef8b0a
mawanquan <1394134553@qq.com>
2018-12-24 15:04:14 +0800

    aaa

--- src/main/java/com/controller/PremeauController.java
+++ src/main/java/com/controller/PremeauController.java
@@ -423,11 +423,12 @@ public class PremeauController {
 	/**
 	 * 部门增删》改获取所有
 	 * 
-	 * @RequestMapping("/upbranch")
+	 *
 	 * @param ids
 	 * @param branchname
 	 * @return
 	 */
+	 @RequestMapping("/upbranch")
 	public String upBranch(Integer ids, String branchname) {
 		Branch branch = new Branch();
 		branch.setId(ids);
